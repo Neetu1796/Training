501315f5e21e57b937ebb2ecf833b2f23f569172
Neetu1796 <40260859+Neetu1796@users.noreply.github.com>
2018-06-29 15:57:34 +0530

    Add files via upload

--- Bing Translator/Bing.java
+++ Bing Translator/Bing.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 
 public class Bing {
@@ -21,30 +22,66 @@ public class Bing {
 				"//div[@id='rich_tltr']/div[@class='t_input']/div[@id='t_inblk']/div[@class='t_txtblkin']/textarea[@id='t_sv']")));
 	}
 
-	public void click_translator() throws NullPointerException {
-		Assert.assertEquals(this.driver.findElement(By.xpath("[@class='t_navlink t_navlinkactive']")).isDisplayed(),
-				true);
-		WebElement text = driver.findElement(By.xpath("[@class='t_navlink t_navlinkactive']"));
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].click();", text);
-	}
+	// public void click_translator() throws NullPointerException {
+	//// Assert.assertEquals(this.driver.findElement(By.xpath("[@class='t_navlink
+	// t_navlinkactive']")).isDisplayed(),
+	//// true);
+	// WebElement text =
+	// driver.findElement(By.xpath("//div[@id='theader']/ul/li/a[@class='t_navlink']"));
+	// JavascriptExecutor js = (JavascriptExecutor) driver;
+	// js.executeScript("arguments[0].click();", text);
+	// }
 
-	public void click_conversion() throws NullPointerException {
-//		Assert.assertEquals(
-//				// this.driver.findElement(By.xpath("[@class ='c-uhf-nav-link f-current
-//				// x-hidden-focus']")).isDisplayed(),
-//				true);// Verification for visiblity of conversion tab WebElement
-		//WebElement conversion = driver.findElement(By.xpath("//div/ul/li[@class=['t_navlink t_navlinkactive']"));
-		WebElement conversion =driver.findElement(By.className("t_navlink t_navlinkactive"));
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].click();", conversion);// clicking conversion
-		js.executeScript("alert('you have clicked coversion tab');");
-	}
+	// public void click_conversion() throws NullPointerException {
+	//// Assert.assertEquals(
+	//// // this.driver.findElement(By.xpath("[@class ='c-uhf-nav-link f-current
+	//// // x-hidden-focus']")).isDisplayed(),
+	//// true);// Verification for visiblity of conversion tab WebElement
+	// //WebElement conversion =
+	// driver.findElement(By.xpath("//div/ul/li[@class=['t_navlink
+	// t_navlinkactive']"));
+	// WebElement conversion =driver.findElement(By.className("t_navlink
+	// t_navlinkactive"));
+	// JavascriptExecutor js = (JavascriptExecutor) driver;
+	// js.executeScript("arguments[0].click();", conversion);// clicking conversion
+	// js.executeScript("alert('you have clicked coversion tab');");
+	// }
 
 	public void search_box() throws NullPointerException {
-		WebElement wb = driver.findElement(By.id("sb_form_q"));
-		wb.sendKeys("google");
-		wb.click();
+		Assert.assertEquals(this.driver.findElement(By.id("sb_form_q")).isDisplayed(), true);
+		// WebElement wb = driver.findElement(By.id("sb_form_q"));
+		// wb.sendKeys("google");
+		// js.executeScript(driver.get, arg1);
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("document.getElementById('sb_form_q').value = 'google';");
+		js.executeScript("document.getElementById('sb_form_q').click();");
+		Assert.assertEquals(this.driver.findElement(By.id("search-button")).isDisplayed(), true);
+		WebElement icon = driver.findElement(By.id("search-button"));
+		js.executeScript("arguments[0].click();", icon);
+		// 2
+		// JavascriptExecutor js =(JavascriptExecutor)driver;
 
+		// wb.click();
+
+	}
+
+	public void drop_down1() {
+		// TODO Auto-generated method stub
+		Assert.assertEquals(this.driver.findElement(By.xpath("//select[@id='t_sl']")).isDisplayed(), true);
+		driver.findElement(By.xpath("//select[@id='t_sl']")).click();
+		
+		driver.findElement(By.id("t_tl")).click();
+	}
+
+	public void set_text() throws NullPointerException {
+		// Assert.assertEquals(this.driver.findElement(By.className("t_txtblkin)")).isDisplayed(),
+		// true);
+		// driver.findElement(By.className("t_txtblkin)")).sendKeys("class");
+		driver.findElement(By.xpath("//div[@id=['t_sv']")).sendKeys("class");
+
+	}
+	public void click_help() {
+		Assert.assertEquals(this.driver.findElement(By.id("sb_help")).isDisplayed(), true);
+		driver.findElement(By.id("sb_help")).click();
 	}
 }
--- Bing Translator/BingTest.java
+++ Bing Translator/BingTest.java
@@ -1,4 +1,5 @@
 package test;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
@@ -6,39 +7,59 @@ import org.testng.annotations.Test;
 
 import tatoctesting.Bing;
 public class BingTest {
-	WebDriver driver;
-	Bing bing;
+	WebDriver driver=new ChromeDriver();
+	Bing bing=new Bing(driver);
+	JavascriptExecutor js=(JavascriptExecutor)driver;
 public BingTest() {
 	System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe");
-	this.driver=new ChromeDriver();
-	Bing bing=new Bing(driver);
+	//this.driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	//Bing bing=new Bing(driver);
 	driver.get("https://www.bing.com/translator");
 }
 @Test
 public void set_connection() {
 	System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32\\chromedriver.exe"); 
-	driver.manage().window().maximize();
+
 	System.out.println("connected");
 	
 }
+//@Test
+//public void click_translator_test()throws NullPointerException {
+//	Bing bing=new Bing(driver);
+//	bing.click_translator();
+//	String expectedurl="https://www.bing.com/translator";
+//	Assert.assertEquals(driver.getCurrentUrl(),expectedurl);
+//}
+//@Test
+//public void click_conversion_test()throws NullPointerException{
+//Bing bing=new Bing(driver);
+//	bing.click_conversion();
+//	String expectedurl="https://translator.microsoft.com/";
+//	Assert.assertEquals(driver.getCurrentUrl(),expectedurl);
+//}
 @Test
-public void click_translator_test()throws NullPointerException {
-	Bing bing=new Bing(driver);
-	bing.click_translator();
+public void search()throws NullPointerException {
+	//Bing bing=new Bing(driver);
+	bing.search_box();
+	//js.executeScript("window.location = 'https://www.bing.com/search?q=google'");
 	String expectedurl="https://www.bing.com/translator";
 	Assert.assertEquals(driver.getCurrentUrl(),expectedurl);
 }
 @Test
-public void click_conversion_test()throws NullPointerException{
-Bing bing=new Bing(driver);
-	bing.click_conversion();
-	String expectedurl="https://translator.microsoft.com/";
-	Assert.assertEquals(driver.getCurrentUrl(),expectedurl);
+public void drop_down1_test() {
+	//Bing bing=new Bing(driver);
+	bing.drop_down1();
 }
 @Test
-public void search()throws NullPointerException {
-	Bing bing=new Bing(driver);
-	bing.search_box();
+public void set_text_test() {
+	bing.set_text();
 	
+}
+@Test
+public void click_help_test() {
+	bing.click_help();
+	String expectedurl="https://www.bing.com/translator";
+	Assert.assertEquals(driver.getCurrentUrl(),expectedurl);
 }
 }
